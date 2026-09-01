53c5edb9395ff2a7bd70fb5605951604de45d834
Fahmid Ferdous <fahmidferdous@gmail.com>
2026-09-01 12:40:59 +0000

    10 upgrades: radar chart, FAQ, metrics strip, cookie consent, reading time, availability badge, typing msg, newsletter, PWA, WhatsApp channel

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'ff-portfolio-v1';
+const ASSETS = ['/', '/index.html', '/photo.jpg', '/og-image.png', '/manifest.json', '/icon-192.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
